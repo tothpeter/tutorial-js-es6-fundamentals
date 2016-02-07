c1d08f718a78908da416086315eaa7bb866b3c6a
Peter Toth <tothpeter08@gmail.com>
2016-02-07 13:23:53 +0100

    2.1 A new Scope

--- js/es6.js
+++ js/es6.js
@@ -1,5 +1,26 @@
 'use strict';
 
 +function(){
-  
+  // console.log(block);
+
+  if (true) {
+    let block = 'block';
+  }
+
+  for (let i = 0; i < 4; i++) {
+    // console.log(i);
+  }
+
+  const PI = 3.14;
+
+  var obj = {
+    test: 'Hello'
+  };
+
+  const ref = obj;
+
+  obj.test = 'changed';
+
+  console.log(ref);
+
 }();
