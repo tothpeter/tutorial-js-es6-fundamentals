3a5dc7fc5b5599b44555ec4de43a94cb1f3251a2
Peter Toth <tothpeter08@gmail.com>
2016-02-10 19:39:49 +0100

    Setup simple watch and auto transpire for the js folder

--- 2.1/gruntfile.js
+++ 2.1/gruntfile.js
@@ -11,9 +11,22 @@ module.exports = function(grunt) {
           'dist/es5.js': 'js/es6.js'
         }
       }
+    },
+
+    watch: {
+      config: {
+        files: ['js/*'],
+        tasks: ['es6'],
+        options: {
+          spawn: false,
+        },
+      },
     }
   });
 
   grunt.loadNpmTasks('grunt-babel');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
   grunt.registerTask('es6', ['babel']);
+  grunt.registerTask('default', ['watch']);
 };
