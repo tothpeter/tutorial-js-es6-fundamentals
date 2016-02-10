53b4e95c00ffcd0eadd5774d2d3a028a1a226536
Peter Toth <tothpeter08@gmail.com>
2016-02-10 21:41:22 +0100

    3.2 new features of the number obj

---
+++ 3.2-new_features-of-the-number-obj/js/es6.js
@@ -0,0 +1,14 @@
+'use strict';
+
++function(){
+  // console.log(Number.EPSILON);
+  //
+  // console.log(Number.isFinite(1));
+  // console.log(Number.isFinite(Infinity));
+  //
+  // console.log(Number.isInteger(1));
+  // console.log(Number.isSafeInteger(Math.pow(2, 53) - 1));
+
+  // console.log(isNaN('NaN'));
+  // console.log(Number.isNaN('NaN'));
+}();
